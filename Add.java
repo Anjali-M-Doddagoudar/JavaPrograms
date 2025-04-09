9d78877abc830d539180d70834dce35c50c755ad
deepajadhav1107 <deepajadhav2001@gmail.com>
2025-04-09 09:30:05 +0530

    Add java file

---
+++ Add.java
@@ -0,0 +1,14 @@
+package JavaPrograms;
+
+public class Add {
+
+    public static void main(String[] args){
+
+        int a=10;
+        int b=3;
+        int sum=a+b;
+
+        system.out.println("Sum"+sum);
+    }
+    
+}
